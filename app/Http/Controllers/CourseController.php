dee8f995c325a890b56e15bcf4055ce7731bab3f
Yonathan Sihotang <143815443+sihotang-yonathan1@users.noreply.github.com>
2025-04-02 07:46:46 +0800

    Initial commit

---
+++ app/Http/Controllers/CourseController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Course;
+use Illuminate\Http\Request;
+
+class CourseController extends Controller
+{
+    //
+    public function get_all_course(Request $request){
+        $semesterFilter = $request->query("semester", null);
+        $course = Course::all();
+        return response()->json($course);
+    }
+
+    public function create_course(Request $request){
+        $course = new Course();
+        $course->name = $request->name;
+        $course->description = $request->description;
+
+        $course->save();
+        return response()->json($course);
+    }
+
+    public function delete_course_by_id(int $course_id){
+        $selected_course = Course::where("id", "=", $course_id)->delete();
+        return response()->json(Course::all());
+    }
+
+    public function update_course_by_id(Request $request, int $course_id){
+        $selected_course = Course::find($course_id);
+        $selected_course->name = $request->name ?? $selected_course->name;
+        $selected_course->description = $request->description ?? $selected_course->description;
+
+        $selected_course->save();
+
+        return response()->json($selected_course);
+    }
+
+    public function get_course_by_id(Request $request, int $course_id){
+        $selected_course = Course::where("id", "=", $course_id);
+        return response()->json($selected_course->get());
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\CourseController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+// Course
+Route::get('/course', [CourseController::class, 'get_all_course']);
+Route::post('/course', [CourseController::class, 'create_course']);
+
+// Course by id
+Route::get('/course/{course_id}', [CourseController::class, 'get_course_by_id']);
+Route::delete('/course/{course_id}', [CourseController::class, 'delete_course_by_id']);
+Route::patch('/course/{course_id}', [CourseController::class, 'update_course_by_id']);
+
+
