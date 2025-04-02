df0b235327a3928b370d437bbfdd37ba4f72c929
Yonathan Sihotang <143815443+sihotang-yonathan1@users.noreply.github.com>
2025-04-02 12:20:12 +0800

    [Controller/CourseController] Set newly defined column can be updated

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -29,16 +29,21 @@ class CourseController extends Controller
     }
 
     public function update_course_by_id(Request $request, int $course_id){
+        // Select the course
         $selected_course = Course::find($course_id);
+        
+        // Update field
         $selected_course->name = $request->name ?? $selected_course->name;
         $selected_course->description = $request->description ?? $selected_course->description;
+        $selected_course->semester = $request->semester ?? $selected_course->semester;
+        $selected_course->credit = $request->credit ?? $selected_course->credit;
 
         $selected_course->save();
 
         return response()->json($selected_course);
     }
 
-    public function get_course_by_id(Request $request, int $course_id){
+    public function get_course_by_id(int $course_id){
         $selected_course = Course::where("id", "=", $course_id);
         return response()->json($selected_course->get());
     }
