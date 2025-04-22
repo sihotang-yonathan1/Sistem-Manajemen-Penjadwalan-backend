b1cffa2c0f8e311c1a6b43687b75e4e4f82164e2
Yonathan Sihotang <143815443+sihotang-yonathan1@users.noreply.github.com>
2025-04-22 12:28:31 +0800

    route: add POST method for `/room`

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,5 @@ Route::patch('/course/{course_id}', [CourseController::class, 'update_course_by_
 
 // Room
 Route::get('/room', [RoomController::class, 'get_all_rooms']);
+Route::post('/room', [RoomController::class, 'create_room']);
 
