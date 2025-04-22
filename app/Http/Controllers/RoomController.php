b522bb6539cd6c8f055821dafa577d36172e75e7
Yonathan Sihotang <143815443+sihotang-yonathan1@users.noreply.github.com>
2025-04-22 12:28:05 +0800

    controller: Add `create_room` for `RoomController`

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -11,4 +11,16 @@ class RoomController extends Controller
         $rooms = Room::all();
         return response()->json($rooms);
     }
+
+    public function create_room(Request $request){
+        $room = new Room();
+        $room->name = $request->name;
+        $room->capacity = $request->capacity;
+        $room->location = $request->location;
+        $room->status = $request->status ?? 'available';
+
+        $room->save();
+        // TODO: set the message
+        return response()->json($room);
+    }
 }
