c40f4424b73e0a0f462fa6f3fa00aa75a129cdb3
Yonathan Sihotang <143815443+sihotang-yonathan1@users.noreply.github.com>
2025-04-22 12:39:28 +0800

    controller: Add `update_room_by_id` in `RoomController`

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -23,4 +23,16 @@ class RoomController extends Controller
         // TODO: set the message
         return response()->json($room);
     }
+
+    public function update_room_by_id(Request $request, int $course_id){
+        $selected_room = Room::find($course_id);
+
+        $selected_room->name = $request->name ?? $selected_room->name;
+        $selected_room->capacity = $request->capacity ?? $selected_room->capacity ?? 0;
+        $selected_room->location = $request->location ?? $selected_room->location;
+        $selected_room->status = $request->status ?? $selected_room->status ?? 'available';
+
+        $selected_room->save();
+        return response()->json($selected_room);
+    }
 }
