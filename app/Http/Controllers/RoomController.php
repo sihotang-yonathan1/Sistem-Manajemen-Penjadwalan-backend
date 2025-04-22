66b21948690ecb9bc834f36d1e4273c237b1b049
sihotang-yonathan1 <143815443+sihotang-yonathan1@users.noreply.github.com>
2025-04-22 12:48:21 +0800

    feat: Add Room model (#1)

---
+++ app/Http/Controllers/RoomController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Room;
+use Illuminate\Http\Request;
+
+class RoomController extends Controller
+{
+    public function get_all_rooms(){
+        $rooms = Room::all();
+        return response()->json($rooms);
+    }
+
+    public function create_room(Request $request){
+        $room = new Room();
+        $room->name = $request->name;
+        $room->capacity = $request->capacity;
+        $room->location = $request->location;
+        $room->status = $request->status ?? 'available';
+
+        $room->save();
+        // TODO: set the message
+        return response()->json($room);
+    }
+
+    public function update_room_by_id(Request $request, int $room_id){
+        $selected_room = Room::find($room_id);
+
+        $selected_room->name = $request->name ?? $selected_room->name;
+        $selected_room->capacity = $request->capacity ?? $selected_room->capacity ?? 0;
+        $selected_room->location = $request->location ?? $selected_room->location;
+        $selected_room->status = $request->status ?? $selected_room->status ?? 'available';
+
+        $selected_room->save();
+        return response()->json($selected_room);
+    }
+
+    public function delete_room_by_id(int $room_id){
+        $selected_room = Room::find($room_id);
+        $selected_room->delete();
+
+        return response()->json([
+            "message" => "Delete room success"
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CourseController;
+use App\Http\Controllers\RoomController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,4 +19,11 @@ Route::get('/course/{course_id}', [CourseController::class, 'get_course_by_id'])
 Route::delete('/course/{course_id}', [CourseController::class, 'delete_course_by_id']);
 Route::patch('/course/{course_id}', [CourseController::class, 'update_course_by_id']);
 
+// Room
+Route::get('/room', [RoomController::class, 'get_all_rooms']);
+Route::post('/room', [RoomController::class, 'create_room']);
+
+// Room by is
+Route::patch('/room/{room_id}', [RoomController::class, 'update_room_by_id']);
+Route::delete('/room/{room_id}', [RoomController::class, 'delete_room_by_id']);
 
