be2b39a5ee3853a97568d8ef502fdf7bab5ffed1
Yonathan Sihotang <143815443+sihotang-yonathan1@users.noreply.github.com>
2025-05-22 12:12:25 +0800

    feat: Add `logout` in `LoginController`

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -34,4 +34,17 @@ class LoginController extends Controller
         ]);
 
     }
+
+    public function logout(Request $request)
+    {
+
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return response()->json([
+            "message" => "Successfully logout"
+        ]);
+    }
 }
